dcfc6ebfe743f3576ff7b48f6b36557843fe39a1
njimenezcro <njimenezcro@hotmail.com>
2021-10-23 02:38:04 -0500

    Cambio Rutas Heroku1

--- src/services/UsersService.js
+++ src/services/UsersService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//const usersUrl = "https://peaceful-peak-03211.herokuapp.com/usuarios"; //prod
-const usersUrl = "http://localhost:3002/usuarios";
+const usersUrl = "https://radiant-headland-05086.herokuapp.com/usuarios"; //prod
+//const usersUrl = "http://localhost:3002/usuarios";//dllo
 
 export const createUser = async (user) => {
     return await axios.post(`${usersUrl}/`, user);
