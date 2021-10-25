158cd7d2996bdae570e13f9e897c2396cb6fe319
njimenezcro <njimenezcro@hotmail.com>
2021-10-25 11:25:13 -0500

    Cambio de Ruta para Despliegue

--- src/services/UsersService.js
+++ src/services/UsersService.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { authHeaders } from "./AuthService";
 
-//const usersUrl = "https://radiant-headland-05086.herokuapp.com/usuarios"; //prod
-const usersUrl = "http://localhost:3002/usuarios";//dllo
+const usersUrl = "https://radiant-headland-05086.herokuapp.com/usuarios"; //prod
+//const usersUrl = "http://localhost:3002/usuarios";//dllo
 
 export const createUser = async (user) => {
     return await axios.post(`${usersUrl}/`, user);
